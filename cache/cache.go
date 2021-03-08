e33d8bdae967e6f1fe36fe2da89c51c8474ca55f
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-03-08 14:57:56 -0600

    Fix cache to handle Set/Get with byte slices appropriately.

--- cache/cache.go
+++ cache/cache.go
@@ -7,8 +7,12 @@
 package cache
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"reflect"
+	"strings"
 	"time"
 
 	"github.com/btm6084/gojson"
@@ -16,8 +20,9 @@ import (
 )
 
 var (
-	c   Cacher
-	dur time.Duration
+	c         Cacher
+	dur       time.Duration
+	deB64Hint = ":::deB64"
 
 	// ErrCacheNil is returned when caching falied due to the provided Cacher being nil
 	ErrCacheNil = errors.New("cache is nil")
@@ -83,15 +88,55 @@ func Get(r metrics.Recorder, key string, container interface{}) error {
 		return ErrDeserialize
 	}
 
-	err = gojson.Unmarshal([]byte(b), &container)
-	if err != nil {
-		return err
-	}
+	switch container.(type) {
+	case []byte, *[]byte:
 
-	return nil
+		if !strings.HasSuffix(b, deB64Hint) {
+			err = gojson.Unmarshal([]byte(b), &container)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		p := reflect.ValueOf(container)
+		if p.Kind() != reflect.Ptr {
+			return fmt.Errorf("supplied container must be a pointer")
+		}
+
+		p = resolvePtr(p)
+		if !p.CanSet() {
+			return fmt.Errorf("unsettable value provided to Get")
+		}
+
+		b = strings.TrimSuffix(b, deB64Hint)
+
+		sq := stripQuotes(b)
+		d := make([]byte, base64.StdEncoding.DecodedLen(len(sq)))
+		n, err := base64.StdEncoding.Decode(d, sq)
+
+		if err != nil {
+			return err
+		}
+		p.SetBytes(d[:n])
+
+		return nil
+	default:
+		if strings.HasSuffix(b, deB64Hint) {
+			b = b64DecodeString(strings.TrimSuffix(b, deB64Hint))
+		}
+
+		err = gojson.Unmarshal([]byte(b), &container)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
 }
 
-// Set a value in cache.
+// Set a value in cache. Value MUST be serializeable by JSON. UnExported fields will be ignored!
 func Set(m metrics.Recorder, key string, value interface{}) error {
 	if !Enabled {
 		return ErrCacheDisabled
@@ -100,7 +145,7 @@ func Set(m metrics.Recorder, key string, value interface{}) error {
 	return SetWithDuration(m, key, value, dur)
 }
 
-// SetWithDuration sets a value in cache.
+// SetWithDuration sets a value in cache. Value MUST be serializeable by JSON. UnExported fields will be ignored!
 func SetWithDuration(m metrics.Recorder, key string, value interface{}, d time.Duration) error {
 	if !Enabled {
 		return ErrCacheDisabled
@@ -110,16 +155,89 @@ func SetWithDuration(m metrics.Recorder, key string, value interface{}, d time.D
 		return ErrCacheNil
 	}
 
-	v := value
-	if d, ok := value.([]byte); ok {
-		v = string(d)
-	}
-
-	raw, err := json.Marshal(v)
+	raw, err := json.Marshal(value)
 	if err != nil {
 		return err
 	}
 
-	c.SetWithDuration(m, key, string(raw), d)
+	hint := ""
+	switch value.(type) {
+	case []byte, *[]byte:
+		hint = deB64Hint
+	}
+
+	c.SetWithDuration(m, key, string(raw)+hint, d)
 	return nil
+}
+
+func stripQuotes(in string) []byte {
+	b := []byte(in)
+
+	if len(b) < 2 {
+		return b
+	}
+
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		b = b[1 : len(b)-1]
+	}
+
+	return b
+}
+
+func b64DecodeString(in string) string {
+	b := []byte(in)
+
+	if len(b) < 2 {
+		return in
+	}
+
+	reQuote := false
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		reQuote = true
+		b = b[1 : len(b)-1]
+	}
+
+	out, err := base64.StdEncoding.DecodeString(string(b))
+	if err != nil {
+		return in
+	}
+
+	if reQuote {
+		return `"` + string(out) + `"`
+	}
+
+	return string(out)
+}
+
+// Resolve a pointer to a concrete Value. If necessary, memory will be allocated to
+// store the object being pointed to.
+func resolvePtr(p reflect.Value) reflect.Value {
+	op := p
+
+	for p.Kind() == reflect.Ptr || p.Kind() == reflect.Interface {
+		if p.Kind() == reflect.Ptr && !p.Elem().CanAddr() {
+			child := reflect.New(p.Type().Elem()).Elem()
+			p.Set(child.Addr())
+		}
+
+		if !p.Elem().IsValid() {
+			break
+		}
+
+		p = p.Elem()
+
+		// Retain the last setable value. This usually comes into play when we have
+		// an interface that represents a non-settable value. The end result is we will
+		// perform the extraction as if we were an interface. This is in alignment with
+		// the behavior of encoding/json.Unmarshal
+		if p.CanSet() {
+			op = p
+		}
+	}
+
+	if !p.CanSet() {
+		p = op
+	}
+
+	return p
 }
