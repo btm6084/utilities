d30e9722153598225a8b4c29664107f1780cfa86
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-03-10 18:24:04 -0600

    Upgrade config package

--- config/config.go
+++ config/config.go
@@ -1,21 +1,23 @@
 package config
 
 import (
+	"context"
 	"encoding/json"
 	"os"
+	"time"
 
+	"github.com/btm6084/godb"
 	"github.com/btm6084/gojson"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
-	config *gojson.JSONReader
-
-	// BaseConfig is the default configuration.
+	// config is considered to be a singleton across an entire program.
 	// Configuration that is expected to be overridden by environment variables should be at the root level
 	// and be simple values. Secrets should (probably) come from the environment and not be comitted with code.
-	BaseConfig = map[string]interface{}{}
+	config *gojson.JSONReader
 
-	// EnvMap maps a BaseConfig key onto the Environmental Key that overrides it.
+	// env maps a BaseConfig key onto the Environmental Key that overrides it.
 	// Environmental configurations must be strings in the BaseConfig, though you can use the Get*
 	// to convert to a desired type.
 	// Environmental configs are those things which are:
@@ -27,73 +29,130 @@ var (
 	// "defaultTimeout":    "TIMEOUT",
 	// "env":               "ENV",
 	// "dbConnectString":   "DB_CONNECT_STRING",
-	EnvMap = map[string]string{}
-
 	env map[string]string
+
+	// fetcher is used to retrieve override configs and a response to a query is expected to return JSON.
+	fetcher godb.JSONFetcher
+
+	// updateStopSignal stops the updater when written to. Only a single updater is allowed to run at a time.
+	updateStopSignal = make(chan bool, 1)
+	updating         bool
+
+	defaultTimeout = 10 * time.Second
 )
 
-func init() {
-	c, err := json.Marshal(BaseConfig)
+// fetcher is used to retrieve override configs and a response to a query is expected to return JSON.
+// If fetcher is nil, that functionality is disabled, and only baseConfig and envMap will be used.
+//
+//
+// BaseConfig is the default configuration.
+// Configuration that is expected to be overridden by environment variables should be at the root level
+// and be simple values. Secrets should (probably) come from the environment and not be comitted with code.
+//
+// EnvMap maps a BaseConfig key onto the Environmental Key that overrides it.
+// Environmental configurations must be strings in the BaseConfig, though you can use the Get*
+// to convert to a desired type.
+// Environmental configs are those things which are:
+//     * Vital to the functionality of the service; or
+//     * Secret in nature
+// ex:
+// "host":              "HOST"
+// "port":              "PORT"
+// "defaultTimeout":    "TIMEOUT"
+// "env":               "ENV"
+// "dbConnectString":   "DB_CONNECT_STRING"
+func Init(f godb.JSONFetcher, configFetchPath string, baseConfig map[string]string, envMap map[string]string, updateFrequency time.Duration) error {
+	if updating {
+		updateStopSignal <- true
+		updating = false
+	}
+
+	err := update(f, configFetchPath, baseConfig, envMap)
 	if err != nil {
-		panic(err)
+		return err
+	}
+
+	go updater(f, configFetchPath, baseConfig, envMap, updateFrequency)
+
+	return nil
+}
+
+// StopUpdates sends a stop signal if there is a currently running updater.
+func StopUpdates() {
+	if updating {
+		updateStopSignal <- true
+		updating = false
+	}
+}
+
+func updater(f godb.JSONFetcher, configFetchPath string, baseConfig map[string]string, envMap map[string]string, updateFrequency time.Duration) error {
+	updating = true
+	ticker := time.NewTicker(500 * time.Millisecond)
+
+	for {
+		select {
+		case <-updateStopSignal:
+			return nil
+		case <-ticker.C:
+			err := update(f, configFetchPath, baseConfig, envMap)
+			if err != nil {
+				log.WithFields(log.Fields{"error": err}).Error("configuration update failed")
+			}
+		}
+	}
+}
+
+func update(f godb.JSONFetcher, configFetchPath string, baseConfig map[string]string, envMap map[string]string) error {
+	var base, fetched, envConfig []byte
+	var err error
+
+	base, err = json.Marshal(baseConfig)
+	if err != nil {
+		return err
 	}
 
 	env = make(map[string]string)
-	for cName, envKey := range EnvMap {
+	for cName, envKey := range envMap {
 		envVal := os.Getenv(envKey)
 		if envVal != "" {
 			env[cName] = envVal
 		}
 	}
 
-	envJSON, err := json.Marshal(env)
-	if err != nil {
-		panic(err)
+	// Merge base <- fetched <- env
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	if fetcher != nil {
+		fetched, err = fetcher.FetchJSON(ctx, configFetchPath)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 
-	b, err := gojson.MergeJSON(c, envJSON)
+	envConfig, err = json.Marshal(env)
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
-	config, err = gojson.NewJSONReader(b)
+	c := merge(merge(base, fetched), envConfig)
+	config, err = gojson.NewJSONReader(c)
 	if err != nil {
-		panic(err)
-	}
-}
-
-// Merge takes a json string and merges it into the current config.
-func Merge(in []byte) error {
-	var original *gojson.JSONReader = config
-
-	envJSON, err := json.Marshal(env)
-	if err != nil {
-		return err
-	}
-
-	c, err := json.Marshal(BaseConfig)
-	if err != nil {
-		return err
-	}
-
-	// Merge CurrentConfig <- IncomingConfig <- EnvironmentalConfig
-	b, err := gojson.MergeJSON(c, in)
-	if err != nil {
-		return err
-	}
-
-	b, err = gojson.MergeJSON(b, envJSON)
-	if err != nil {
-		return err
-	}
-
-	config, err = gojson.NewJSONReader(b)
-	if err != nil {
-		*config = *original
 		return err
 	}
 
 	return nil
+}
+
+// merge b on top of a; get a back if it fails.
+func merge(a, b []byte) []byte {
+	out, err := gojson.MergeJSON(a, b)
+	if err != nil {
+		log.Println(err)
+		return a
+	}
+
+	return out
 }
 
 // GetString returns the value of the given configuration element at the specified key as a string,
