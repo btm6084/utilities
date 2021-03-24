9467969cabf442499822c6e2b0c911a641acc489
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-03-24 11:24:28 -0500

    Update config to be more useable.

--- config/config.go
+++ config/config.go
@@ -13,180 +13,9 @@ import (
 )
 
 var (
-	rawConfig []byte
-
-	// config is considered to be a singleton across an entire program.
-	// Configuration that is expected to be overridden by environment variables should be at the root level
-	// and be simple values. Secrets should (probably) come from the environment and not be comitted with code.
-	config *gojson.JSONReader
-
-	// env maps a BaseConfig key onto the Environmental Key that overrides it.
-	// Environmental configurations must be strings in the BaseConfig, though you can use the Get*
-	// to convert to a desired type.
-	// Environmental configs are those things which are:
-	//     * Vital to the functionality of the service; or
-	//     * Secret in nature
-	// ex:
-	// "host":              "HOST",
-	// "port":              "PORT",
-	// "defaultTimeout":    "TIMEOUT",
-	// "env":               "ENV",
-	// "dbConnectString":   "DB_CONNECT_STRING",
-	env map[string]string
-
-	// updateStopSignal stops the updater when written to. Only a single updater is allowed to run at a time.
-	updateStopSignal = make(chan bool, 1)
-	updating         bool
-
-	defaultTimeout = 10 * time.Second
-
 	ErrNoConfig     = errors.New("no config available. did you call init?")
 	ErrNotValidJSON = errors.New("base config must be valid json")
 )
-
-// fetcher is used to retrieve override configs and a response to a query is expected to return JSON.
-// If fetcher is nil, that functionality is disabled, and only baseConfig and envMap will be used.
-//
-//
-// BaseConfig is the default configuration.
-// Configuration that is expected to be overridden by environment variables should be at the root level
-// and be simple values. Secrets should (probably) come from the environment and not be comitted with code.
-//
-// EnvMap maps an environment key onto the key in basic config to override.
-// Environmental configurations must be strings in the BaseConfig, though you can use the Get*
-// to convert to a desired type.
-// Environmental configs are those things which are:
-//     * Vital to the functionality of the service; or
-//     * Secret in nature
-// ex:
-// "HOST":              "host"
-// "PORT":              "port"
-// "TIMEOUT":           "defaultTimeout"
-// "ENV":               "env"
-// "DB_CONNECT_STRING": "dbConnectString"
-func Init(f godb.JSONFetcher, configFetchPath string, baseConfig []byte, envMap map[string]string, updateFrequency time.Duration) error {
-	if !gojson.IsJSON(baseConfig) {
-		return ErrNotValidJSON
-	}
-
-	rawConfig = baseConfig
-
-	if updating {
-		updateStopSignal <- true
-		updating = false
-	}
-
-	err := update(f, configFetchPath, envMap)
-	if err != nil {
-		return err
-	}
-
-	if updateFrequency > 0 {
-		go updater(f, configFetchPath, envMap, updateFrequency)
-	}
-
-	return nil
-}
-
-// SetConfig specifies a configuration value with no updating.
-func SetConfig(b []byte) error {
-	StopUpdates()
-
-	rawConfig = b
-
-	var err error
-	config, err = gojson.NewJSONReader(rawConfig)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// StopUpdates sends a stop signal if there is a currently running updater.
-func StopUpdates() {
-	if updating {
-		updateStopSignal <- true
-		updating = false
-	}
-}
-
-func updater(f godb.JSONFetcher, configFetchPath string, envMap map[string]string, updateFrequency time.Duration) error {
-	updating = true
-	ticker := time.NewTicker(updateFrequency)
-
-	for {
-		select {
-		case <-updateStopSignal:
-			return nil
-		case <-ticker.C:
-			err := update(f, configFetchPath, envMap)
-			if err != nil {
-				log.WithFields(log.Fields{
-					"error":   err,
-					"package": "github.com/btm6084/utilities/config",
-				}).Error("configuration update failed")
-			}
-		}
-	}
-}
-
-func update(f godb.JSONFetcher, configFetchPath string, envMap map[string]string) error {
-	var fetched, envConfig []byte
-	var err error
-
-	fields := log.Fields{
-		"package": "github.com/btm6084/utilities/config",
-	}
-
-	env = make(map[string]string)
-	for envKey, cName := range envMap {
-		envVal := os.Getenv(envKey)
-		if envVal != "" {
-			env[cName] = envVal
-		}
-	}
-
-	// Merge base <- fetched <- env
-	if f != nil {
-		ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-		defer cancel()
-
-		fetched, err = f.FetchJSON(ctx, configFetchPath)
-		if err != nil {
-			fields["context"] = "update FetchJSON"
-			log.WithFields(fields).Println(err)
-		}
-	}
-
-	envConfig, err = json.Marshal(env)
-	if err != nil {
-		fields["context"] = "update marshal env"
-		log.WithFields(fields).Println(err)
-	}
-
-	if !gojson.IsJSON(rawConfig) {
-		rawConfig = []byte(`{}`)
-	}
-
-	if !gojson.IsJSON(fetched) {
-		fetched = []byte(`{}`)
-	}
-
-	if !gojson.IsJSON(envConfig) {
-		envConfig = []byte(`{}`)
-	}
-
-	c := merge(merge(rawConfig, fetched), envConfig)
-	config, err = gojson.NewJSONReader(c)
-	if err != nil {
-		return err
-	}
-
-	rawConfig = c
-
-	return nil
-}
 
 // merge b on top of a; get a back if it fails.
 func merge(a, b []byte) []byte {
@@ -199,122 +28,130 @@ func merge(a, b []byte) []byte {
 	return out
 }
 
-// GetString returns the value of the given configuration element at the specified key as a string,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetString(key string) string {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return ""
-	}
+type Configuration struct {
+	Config    *gojson.JSONReader
+	RawConfig []byte
+	EnvMap    map[string]string
 
-	return config.GetString(key)
+	updateStopSignal chan bool
+	updating         bool
+	updateFrequency  time.Duration
 }
 
-// GetInt returns the value of the given configuration element at the specified key as an int,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetInt(key string) int {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return 0
+func NewLocalConfiguration(baseConfig []byte, envMap map[string]string) (*Configuration, error) {
+	c := &Configuration{
+		RawConfig:        baseConfig,
+		EnvMap:           envMap,
+		updating:         false,
+		updateStopSignal: make(chan bool, 1),
+		updateFrequency:  0,
 	}
 
-	return config.GetInt(key)
+	err := c.update(nil, "")
+	return c, err
 }
 
-// GetBool returns the value of the given configuration element at the specified key as a bool,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetBool(key string) bool {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return false
+func NewRemoteConfiguration(baseConfig []byte, envMap map[string]string, f godb.JSONFetcher, settingsPath string, updateFrequency time.Duration) (*Configuration, error) {
+	if !gojson.IsJSON(baseConfig) {
+		return nil, ErrNotValidJSON
 	}
 
-	return config.GetBool(key)
+	c := &Configuration{
+		RawConfig:        baseConfig,
+		EnvMap:           envMap,
+		updating:         false,
+		updateStopSignal: make(chan bool, 1),
+		updateFrequency:  updateFrequency,
+	}
+
+	err := c.update(f, settingsPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.updateFrequency > 0 {
+		go c.updater(f, settingsPath)
+	}
+
+	return c, nil
 }
 
-// GetFloat returns the value of the given configuration element at the specified key as a float64,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetFloat(key string) float64 {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return 0.0
-	}
+func (c *Configuration) updater(f godb.JSONFetcher, path string) error {
+	c.updating = true
+	ticker := time.NewTicker(c.updateFrequency)
 
-	return config.GetFloat(key)
+	for {
+		select {
+		case <-c.updateStopSignal:
+			return nil
+		case <-ticker.C:
+			err := c.update(f, path)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"error":   err,
+					"package": "github.com/btm6084/utilities/config",
+				}).Error("configuration update failed")
+			}
+		}
+	}
 }
 
-// GetMapStringBool returns the value of the given configuration element at the specified key as map[string]bool,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetMapStringBool(key string) map[string]bool {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return nil
+func (c *Configuration) update(f godb.JSONFetcher, path string) error {
+	var fetched, envConfig []byte
+	var err error
+
+	fields := log.Fields{
+		"package": "github.com/btm6084/utilities/config",
 	}
 
-	return config.Get(key).ToMapStringBool()
+	env := make(map[string]string)
+	for envKey, cName := range c.EnvMap {
+		envVal := os.Getenv(envKey)
+		if envVal != "" {
+			env[cName] = envVal
+		}
+	}
+
+	// Merge base <- fetched <- env
+	if f != nil {
+		// It's assumed that the root url for the fetcher will provide the configuration data.
+		fetched, err = f.FetchJSON(context.Background(), path)
+		if err != nil {
+			fields["context"] = "update FetchJSON"
+			log.WithFields(fields).Println(err)
+		}
+	}
+
+	envConfig, err = json.Marshal(env)
+	if err != nil {
+		fields["context"] = "update marshal env"
+		log.WithFields(fields).Println(err)
+	}
+
+	if !gojson.IsJSON(fetched) {
+		fetched = []byte(`{}`)
+	}
+
+	if !gojson.IsJSON(envConfig) {
+		envConfig = []byte(`{}`)
+	}
+
+	cfg := merge(merge(c.RawConfig, fetched), envConfig)
+	reader, err := gojson.NewJSONReader(cfg)
+	if err != nil {
+		return err
+	}
+
+	c.RawConfig = cfg
+	c.Config = reader
+
+	return nil
 }
 
-// GetMapStringString returns the value of the given configuration element at the specified key as map[string]string,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetMapStringString(key string) map[string]string {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return nil
+// StopUpdates sends a stop signal if there is a currently running updater.
+func (c *Configuration) StopUpdates() {
+	if c.updating {
+		c.updateStopSignal <- true
+		c.updating = false
 	}
-
-	return config.Get(key).ToMapStringString()
-}
-
-// GetMapStringInt returns the value of the given configuration element at the specified key as map[string]int,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetMapStringInt(key string) map[string]int {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return nil
-	}
-
-	return config.Get(key).ToMapStringInt()
-}
-
-// GetMapStringFloat returns the value of the given configuration element at the specified key as map[string]int,
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetMapStringFloat(key string) map[string]float64 {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return nil
-	}
-
-	return config.Get(key).ToMapStringFloat()
-}
-
-// GetMapStringInterface returns the value of the given configuration element at the specified key as map[string]interface{},
-// regardless of the original type at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func GetMapStringInterface(key string) map[string]interface{} {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return nil
-	}
-
-	return config.Get(key).ToMapStringInterface()
-}
-
-// Get returns the value of the given configuration element at the specified key as the JSON Type
-// as it exists at that key.
-// Key is a valid JSON Path (https://goessner.net/articles/JsonPath/)
-func Get(key string) interface{} {
-	if config == nil || len(config.Keys) == 0 {
-		log.Println(ErrNoConfig)
-		return nil
-	}
-
-	return config.GetInterface(key)
 }
