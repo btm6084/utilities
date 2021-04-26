2251967fc28f11bb46e1f00b404fb9e0b476545f
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-04-26 13:24:45 -0500

    Fix the signatures of RDBFromContext and ESFromContext to not require a label (can be added later as needed)

--- dependency/elasticsearch.go
+++ dependency/elasticsearch.go
@@ -7,6 +7,11 @@ import (
 	"github.com/elastic/go-elasticsearch"
 )
 
+var (
+	// For now, we only allow a single es instance per service. If this changes, this can be shifted to a parameter.
+	esLabel = `esInstance`
+)
+
 // ElasticsearchDependencyHandler is a middleware to inject an elasticsearch.Client into context.
 func ElasticsearchDependencyHandler(es *elasticsearch.Client) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -18,27 +23,25 @@ func ElasticsearchDependencyHandler(es *elasticsearch.Client) func(http.Handler)
 
 // ConextWithElasticsearch returns a context containing the provided elasticsearch client.
 func ContextWithElasticsearch(ctx context.Context, es *elasticsearch.Client) context.Context {
-	label := `esInstance`
-
 	esList, ok := ctx.Value(esContextKey).(map[string]*elasticsearch.Client)
 	if !ok || esList == nil {
 		esList = make(map[string]*elasticsearch.Client)
 	}
 
-	esList[label] = es
+	esList[esLabel] = es
 
 	nc := context.WithValue(ctx, esContextKey, esList)
 
 	return nc
 }
 
-func ESFromContext(ctx context.Context, label string) *elasticsearch.Client {
+func ESFromContext(ctx context.Context) *elasticsearch.Client {
 	esList, ok := ctx.Value(esContextKey).(map[string]*elasticsearch.Client)
 	if !ok {
 		return nil
 	}
 
-	if es, isset := esList[label]; isset {
+	if es, isset := esList[esLabel]; isset {
 		return es
 	}
 
--- dependency/redis.go
+++ dependency/redis.go
@@ -7,6 +7,11 @@ import (
 	"github.com/btm6084/utilities/redis"
 )
 
+var (
+	// For now, we only allow a single RDB instance per service. If this changes, this can be shifted to a parameter.
+	rdbLabel = `rdbInstance`
+)
+
 // RedisDependencyHandler is a middleware to inject a redis.Client into context.
 func RedisDependencyHandler(rdb *redis.Client) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -18,27 +23,25 @@ func RedisDependencyHandler(rdb *redis.Client) func(http.Handler) http.Handler {
 
 // ConextWithRedis returns a context containing the provided redis client.
 func ContextWithRedis(ctx context.Context, rdb *redis.Client) context.Context {
-	label := `rdbInstance`
-
 	rdbList, ok := ctx.Value(rdbContextKey).(map[string]*redis.Client)
 	if !ok || rdbList == nil {
 		rdbList = make(map[string]*redis.Client)
 	}
 
-	rdbList[label] = rdb
+	rdbList[rdbLabel] = rdb
 
 	nc := context.WithValue(ctx, rdbContextKey, rdbList)
 
 	return nc
 }
 
-func RDBFromContext(ctx context.Context, label string) *redis.Client {
+func RDBFromContext(ctx context.Context) *redis.Client {
 	rdbList, ok := ctx.Value(rdbContextKey).(map[string]*redis.Client)
 	if !ok {
 		return nil
 	}
 
-	if rdb, isset := rdbList[label]; isset {
+	if rdb, isset := rdbList[rdbLabel]; isset {
 		return rdb
 	}
 
