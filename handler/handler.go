774bfef1e653f6d188b168148b6a0fcb2b9b0e3d
tcgcharles <charles@tcgplayer.com>
2022-01-20 12:26:00 -0600

    CSI-3471 added HEAD handler to be used as a wrapper

--- handler/handler.go
+++ handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -21,4 +22,42 @@ func CreateOptionsHandler(methods ...string) http.HandlerFunc {
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusNoContent)
 	}
+}
+
+// HeadResponseWriter captures input to an http.ResponseWriter
+type HeadResponseWriter struct {
+	Bytes      int
+	StatusCode int
+	w          http.ResponseWriter
+}
+
+// Header proxies http.ResponseWriter Header
+func (w *HeadResponseWriter) Header() http.Header {
+	return w.w.Header()
+}
+
+// WriteHeader proxies http.ResponseWriter WriteHeader
+func (w *HeadResponseWriter) WriteHeader(statusCode int) {
+	w.StatusCode = statusCode
+	w.w.WriteHeader(statusCode)
+}
+
+// Write prevents http.ResponseWriter Write from writing anything to the response body for HEAD requests.
+func (w *HeadResponseWriter) Write(b []byte) (int, error) {
+	w.Bytes += len(b)
+	return w.Bytes, nil
+}
+
+// HeadHandler returns a handler that prevents a body from being returned for any wrapped request handler if the HTTP method is HEAD.
+func HeadHandler(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "HEAD" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		writer := HeadResponseWriter{w: w}
+		next.ServeHTTP(&writer, r)
+		writer.Header().Set("Content-Length", strconv.Itoa(writer.Bytes))
+	})
 }
