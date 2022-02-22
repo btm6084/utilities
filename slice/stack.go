c8ca5e129c5e5bba3c326df9ad3b5b96824a6008
Benjamin Maynor <BenjaminMaynor@gmail.com>
2022-02-22 11:50:18 -0600

    Add simple stack

---
+++ slice/stack.go
@@ -0,0 +1,127 @@
+package slice
+
+import "errors"
+
+var (
+	ErrStackEmpty = errors.New("stack is empty")
+)
+
+type IntStack struct {
+	ints []int
+}
+
+func (s *IntStack) Push(i int) {
+	s.ints = append(s.ints, i)
+}
+
+func (s *IntStack) Pop() (int, error) {
+	if len(s.ints) == 0 {
+		return 0, ErrStackEmpty
+	}
+
+	i := s.ints[len(s.ints)-1]
+	s.ints = s.ints[:len(s.ints)-1]
+	return i, nil
+}
+
+func (s *IntStack) Peek() (int, error) {
+	if len(s.ints) == 0 {
+		return 0, ErrStackEmpty
+	}
+
+	return s.ints[len(s.ints)-1], nil
+}
+
+func (s *IntStack) Len() int {
+	return len(s.ints)
+}
+
+type StringStack struct {
+	strings []string
+}
+
+func (s *StringStack) Push(i string) {
+	s.strings = append(s.strings, i)
+}
+
+func (s *StringStack) Pop() (string, error) {
+	if len(s.strings) == 0 {
+		return "", ErrStackEmpty
+	}
+
+	i := s.strings[len(s.strings)-1]
+	s.strings = s.strings[:len(s.strings)-1]
+	return i, nil
+}
+
+func (s *StringStack) Peek() (string, error) {
+	if len(s.strings) == 0 {
+		return "", ErrStackEmpty
+	}
+
+	return s.strings[len(s.strings)-1], nil
+}
+
+func (s *StringStack) Len() int {
+	return len(s.strings)
+}
+
+type FloatStack struct {
+	floats []float64
+}
+
+func (s *FloatStack) Push(i float64) {
+	s.floats = append(s.floats, i)
+}
+
+func (s *FloatStack) Pop() (float64, error) {
+	if len(s.floats) == 0 {
+		return 0, ErrStackEmpty
+	}
+
+	i := s.floats[len(s.floats)-1]
+	s.floats = s.floats[:len(s.floats)-1]
+	return i, nil
+}
+
+func (s *FloatStack) Peek() (float64, error) {
+	if len(s.floats) == 0 {
+		return 0, ErrStackEmpty
+	}
+
+	return s.floats[len(s.floats)-1], nil
+}
+
+func (s *FloatStack) Len() int {
+	return len(s.floats)
+}
+
+type BoolStack struct {
+	bools []bool
+}
+
+func (s *BoolStack) Push(i bool) {
+	s.bools = append(s.bools, i)
+}
+
+func (s *BoolStack) Pop() (bool, error) {
+	if len(s.bools) == 0 {
+		return false, ErrStackEmpty
+	}
+
+	i := s.bools[len(s.bools)-1]
+	s.bools = s.bools[:len(s.bools)-1]
+	return i, nil
+}
+
+func (s *BoolStack) Peek() (bool, error) {
+	if len(s.bools) == 0 {
+		return false, ErrStackEmpty
+	}
+
+	return s.bools[len(s.bools)-1], nil
+}
+
+func (s *BoolStack) Len() int {
+	return len(s.bools)
+}
---
+++ slice/stack_test.go
@@ -0,0 +1,164 @@
+package slice
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntStack(t *testing.T) {
+	s := IntStack{}
+
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	require.Equal(t, 3, s.Len())
+
+	i, err := s.Pop()
+	require.Equal(t, 3, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, 2, i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, 2, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, 1, i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, 1, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, 0, i)
+	require.Equal(t, ErrStackEmpty, err)
+
+	i, err = s.Pop()
+	require.Equal(t, 0, i)
+	require.Equal(t, ErrStackEmpty, err)
+}
+
+func TestStringStack(t *testing.T) {
+	s := StringStack{}
+
+	s.Push("1")
+	s.Push("2")
+	s.Push("3")
+	require.Equal(t, 3, s.Len())
+
+	i, err := s.Pop()
+	require.Equal(t, "3", i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, "2", i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, "2", i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, "1", i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, "1", i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, "", i)
+	require.Equal(t, ErrStackEmpty, err)
+
+	i, err = s.Pop()
+	require.Equal(t, "", i)
+	require.Equal(t, ErrStackEmpty, err)
+}
+
+func TestFloatStack(t *testing.T) {
+	s := FloatStack{}
+
+	s.Push(1.5)
+	s.Push(2.5)
+	s.Push(3.5)
+	require.Equal(t, 3, s.Len())
+
+	i, err := s.Pop()
+	require.Equal(t, 3.5, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, 2.5, i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, 2.5, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, 1.5, i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, 1.5, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, 0.0, i)
+	require.Equal(t, ErrStackEmpty, err)
+
+	i, err = s.Pop()
+	require.Equal(t, 0.0, i)
+	require.Equal(t, ErrStackEmpty, err)
+}
+
+func TestBoolStack(t *testing.T) {
+	s := BoolStack{}
+
+	s.Push(true)
+	s.Push(false)
+	s.Push(true)
+	s.Push(false)
+	require.Equal(t, 4, s.Len())
+
+	i, err := s.Pop()
+	require.Equal(t, false, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, true, i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, true, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, false, i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, false, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, true, i)
+	require.Nil(t, err)
+
+	i, err = s.Pop()
+	require.Equal(t, true, i)
+	require.Nil(t, err)
+
+	i, err = s.Peek()
+	require.Equal(t, false, i)
+	require.Equal(t, ErrStackEmpty, err)
+
+	i, err = s.Pop()
+	require.Equal(t, false, i)
+	require.Equal(t, ErrStackEmpty, err)
+}
