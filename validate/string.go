0beefe1aa71852f3da27155e71130b198e1fe328
Benjamin Maynor <BenjaminMaynor@gmail.com>
2022-06-06 12:20:41 -0500

    Add Validate Package

---
+++ validate/string.go
@@ -0,0 +1,39 @@
+package validate
+
+import "regexp"
+
+var (
+	// IDNumber matches a single integer, potentially negative.
+	numeric            = regexp.MustCompile("^-?[1-9][0-9]*$")
+	numericLeadingZero = regexp.MustCompile("^-?[0-9]+$")
+
+	// idWord matches a single word-character identifier. Allows spaces.
+	idWord = regexp.MustCompile(`^[A-Za-z_][\w ]*$`)
+
+	alpha    = regexp.MustCompile(`^[A-Za-z]+$`)
+	alphaNum = regexp.MustCompile(`^[A-Za-z0-9]+$`)
+)
+
+func IsInteger(in string, allowLeadingZeros bool) bool {
+	if in == "0" {
+		return true
+	}
+
+	if allowLeadingZeros {
+		return numericLeadingZero.MatchString(in)
+	}
+
+	return numeric.MatchString(in)
+}
+
+func IsIdentifier(in string) bool {
+	return idWord.MatchString(in)
+}
+
+func IsAlpha(in string) bool {
+	return alpha.MatchString(in)
+}
+
+func IsAlphaNum(in string) bool {
+	return alphaNum.MatchString(in)
+}
---
+++ validate/string_test.go
@@ -0,0 +1,133 @@
+package validate
+
+import (
+	"testing"
+
+	"github.com/spf13/cast"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsAlpha(t *testing.T) {
+	var testCases = []struct {
+		Input    string
+		Expected bool
+	}{
+		{"Hello", true},
+		{"lskdhflshdfasiyhdflsakdhflaskdhvasdlfv", true},
+		{"ls12kdhfl3shdfasiyhdfl32213123sakdh2flaskdh3vasdlfv", false},
+		{"lskdhfls h d fasiyhdflsakdhflaskdhvasdlfv", false},
+		{"Hello!", false},
+		{"Alpha Numeric 123", false},
+		{"3728123123", false},
+		{"-3728123123", false},
+		{"-0728123123", false},
+		{"003728123123", false},
+		{"_003728123123", false},
+	}
+
+	for n, tc := range testCases {
+		t.Run("IsAlpha "+cast.ToString(n), func(t *testing.T) {
+			require.Equal(t, tc.Expected, IsAlpha(tc.Input))
+		})
+	}
+}
+
+func TestIsAlphaNumeric(t *testing.T) {
+	var testCases = []struct {
+		Input    string
+		Expected bool
+	}{
+		{"Hello", true},
+		{"lskdhflshdfasiyhdflsakdhflaskdhvasdlfv", true},
+		{"ls12kdhfl3shdfasiyhdfl32213123sakdh2flaskdh3vasdlfv", true},
+		{"lskdhfls h d fasiyhdflsakdhflaskdhvasdlfv", false},
+		{"Hello!", false},
+		{"Alpha Numeric 123", false},
+		{"3728123123", true},
+		{"-3728123123", false},
+		{"-0728123123", false},
+		{"003728123123", true},
+		{"_003728123123", false},
+	}
+
+	for n, tc := range testCases {
+		t.Run("IsAlphaNum "+cast.ToString(n), func(t *testing.T) {
+			require.Equal(t, tc.Expected, IsAlphaNum(tc.Input))
+		})
+	}
+}
+
+func TestIsIntegerNoLeadingZeros(t *testing.T) {
+	var testCases = []struct {
+		Input    string
+		Expected bool
+	}{
+		{"Hello", false},
+		{"lskdhflshdfasiyhdflsakdhflaskdhvasdlfv", false},
+		{"ls12kdhfl3shdfasiyhdfl32213123sakdh2flaskdh3vasdlfv", false},
+		{"lskdhfls h d fasiyhdflsakdhflaskdhvasdlfv", false},
+		{"Hello!", false},
+		{"Alpha Numeric 123", false},
+		{"3728123123", true},
+		{"-3728123123", true},
+		{"-0728123123", false},
+		{"003728123123", false},
+		{"_003728123123", false},
+	}
+
+	for n, tc := range testCases {
+		t.Run("IsIntegerNoZeros "+cast.ToString(n), func(t *testing.T) {
+			require.Equal(t, tc.Expected, IsInteger(tc.Input, false))
+		})
+	}
+}
+
+func TestIsIntegerLeadingZeros(t *testing.T) {
+	var testCases = []struct {
+		Input    string
+		Expected bool
+	}{
+		{"Hello", false},
+		{"lskdhflshdfasiyhdflsakdhflaskdhvasdlfv", false},
+		{"ls12kdhfl3shdfasiyhdfl32213123sakdh2flaskdh3vasdlfv", false},
+		{"lskdhfls h d fasiyhdflsakdhflaskdhvasdlfv", false},
+		{"Hello!", false},
+		{"Alpha Numeric 123", false},
+		{"3728123123", true},
+		{"-3728123123", true},
+		{"-0728123123", true},
+		{"003728123123", true},
+		{"_003728123123", false},
+	}
+
+	for n, tc := range testCases {
+		t.Run("IsIntegerWithZeros "+cast.ToString(n), func(t *testing.T) {
+			require.Equal(t, tc.Expected, IsInteger(tc.Input, true))
+		})
+	}
+}
+
+func TestIsIdentifier(t *testing.T) {
+	var testCases = []struct {
+		Input    string
+		Expected bool
+	}{
+		{"Hello", true},
+		{"lskdhflshdfasiyhdflsakdhflaskdhvasdlfv", true},
+		{"ls12kdhfl3shdfasiyhdfl32213123sakdh2flaskdh3vasdlfv", true},
+		{"lskdhfls h d fasiyhdflsakdhflaskdhvasdlfv", true},
+		{"Hello!", false},
+		{"Alpha Numeric 123", true},
+		{"3728123123", true},
+		{"-3728123123", false},
+		{"-0728123123", false},
+		{"003728123123", false},
+		{"_003728123123", true},
+	}
+
+	for n, tc := range testCases {
+		t.Run("IsIdentifier "+cast.ToString(n), func(t *testing.T) {
+			require.Equal(t, tc.Expected, IsIdentifier(tc.Input))
+		})
+	}
+}
